55fc0404dfa9baa77146f10440c4c11c2da5d7bb
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-11-21 22:56:52 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,11 @@
-// import React, { Component, useState } from "react";
-// import "../styles/App.css";
-
-// let value = 0;
-// const App = (props) => {
-//   let length = props.slides.length;
-
-//   console.log(length);
-
-//   const [title, settitle] = useState(props.slides[value].title);
-
-//   const [text, settext] = useState(props.slides[value].text);
-
-//   const nextClick = () => {
-//     value += 1;
-
-//     if (value <= length || length === 1) {
-//       console.log("nextclick", value);
-//       settitle(props.slides[value].title);
-//       settext(props.slides[value].text);
-//     }
-//   };
-//   const prevClick = () => {
-//     value -= 1;
-//     if (value >= 0 && value <= length) {
-//       console.log("preClick", value);
-//       settitle(props.slides[value].title);
-//       settext(props.slides[value].text);
-//     }
-//   };
-
-//   const restartClick = () => {
-//     console.log("resertClick", value);
-//     settitle(props.slides[0].title);
-//     settext(props.slides[0].text);
-//     value = 0;
-//   };
-
-//   return (
-//     <>
-//       <h1 data-testid="title">{title}</h1>
-//       <p data-testid="text">{text}</p>
-
-//       <button
-//         id="prev"
-//         data-testid="button-prev"
-//         disabled={value === 0 ? true : false}
-//         onClick={prevClick}
-//       >
-//         Prev
-//       </button>
-
-//       <button
-//         id="next"
-//         data-testid="button-next"
-//         disabled={value === length - 1 ? true : false}
-//         onClick={nextClick}
-//       >
-//         Next
-//       </button>
-
-//       <button
-//         id="resert"
-//         data-testid="button-restart"
-//         disabled={value === 0 ? true : false}
-//         onClick={restartClick}
-//       >
-//         Restart
-//       </button>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+
 let value = 0;
 const App = (props) => {
   let length = props.slides.length;
 
-  // console.log(length);
+  console.log(length);
 
   const [title, settitle] = useState(props.slides[value].title);
 
@@ -90,7 +15,7 @@ const App = (props) => {
     value += 1;
 
     if (value <= length || length === 1) {
-      // console.log("nextclick", value);
+      console.log("nextclick", value);
       settitle(props.slides[value].title);
       settext(props.slides[value].text);
     }
@@ -98,17 +23,17 @@ const App = (props) => {
   const prevClick = () => {
     value -= 1;
     if (value >= 0 && value <= length) {
-      // console.log("preClick", value);
+      console.log("preClick", value);
       settitle(props.slides[value].title);
       settext(props.slides[value].text);
     }
   };
 
   const restartClick = () => {
+    console.log("resertClick", value);
+    settitle(props.slides[0].title);
+    settext(props.slides[0].text);
     value = 0;
-    // console.log("resertClick", value);
-    settitle(props.slides[value].title);
-    settext(props.slides[value].text);
   };
 
   return (
@@ -117,27 +42,24 @@ const App = (props) => {
       <p data-testid="text">{text}</p>
 
       <button
-        id="prev"
         data-testid="button-prev"
-        disabled={value === 0}
+        disabled={value === 0 ? true : false}
         onClick={prevClick}
       >
         Prev
       </button>
 
       <button
-        id="next"
         data-testid="button-next"
-        disabled={value === length - 1}
+        disabled={value === length - 1 ? true : false}
         onClick={nextClick}
       >
         Next
       </button>
 
       <button
-        id="resert"
         data-testid="button-restart"
-        disabled={value === 0}
+        disabled={value === 0 ? true : false}
         onClick={restartClick}
       >
         Restart
